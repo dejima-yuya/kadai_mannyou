fa2908e213d3ae8d1b95b560fe7eec528dd5aae5
dejima-yuya <jimajapan33@yahoo.co.jp>
2023-06-26 14:42:39 +0900

    タスクの新規作成機能を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,6 +3,15 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+  end
+
   def show
     @task = Task.find(params[:id])
   end
