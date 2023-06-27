07485640704eb53ae7fc44e5adc035129632d87e
dejima-yuya <jimajapan33@yahoo.co.jp>
2023-06-27 11:50:40 +0900

    タスクの作成後と編集後にタスク一覧画面に遷移させるため

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,7 +10,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-      redirect_to new_task_path, notice: "タスクを作成しました！"
+      redirect_to tasks_path, notice: "タスクを作成しました！"
     else
       render :new
     end
@@ -27,7 +27,7 @@ class TasksController < ApplicationController
   def update
     @task = Task.find(params[:id])
     if @task.update(task_params)
-      redirect_to edit_task_path, notice: "タスクを編集しました！"
+      redirect_to tasks_path, notice: "タスクを編集しました！"
     else
       render :edit
     end
