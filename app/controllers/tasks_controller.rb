7caa865e411a9ec6ffb6245397e46e5b184132bf
dejima-yuya <jimajapan33@yahoo.co.jp>
2023-06-30 13:32:24 +0900

    タスクの新規作成時に終了期限も登録できるようにするため

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -42,6 +42,6 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:title, :content)
+    params.require(:task).permit(:title, :content, :end_date)
   end
 end
