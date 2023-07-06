5a29753e758d1f5cd98654ebc6c9ff33324246a2
dejima-yuya <jimajapan33@yahoo.co.jp>
2023-07-06 13:34:18 +0900

    task作成時にstatusの値をプルダウンで選択し、登録できる機能を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -46,6 +46,6 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:title, :content, :end_date)
+    params.require(:task).permit(:title, :content, :end_date, :status)
   end
 end
