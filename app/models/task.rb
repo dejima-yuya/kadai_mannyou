22b481215bdb161b40f8d089f05706952dd5a236
dejima-yuya <jimajapan33@yahoo.co.jp>
2023-07-06 13:41:05 +0900

    statusカラムにバリデーションを設定

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,5 @@ class Task < ApplicationRecord
   validates :title,  presence: true, length: { maximum: 30 }
   validates :content, presence: true
   validates :end_date, presence: true
+  validates :status, presence: true
 end
