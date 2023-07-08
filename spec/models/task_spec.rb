d66230ad8834135a6f49d63691e6801d67e3f9fb
dejima-yuya <jimajapan33@yahoo.co.jp>
2023-07-08 19:34:49 +0900

    end_dateカラムと statusカラムを追加したので、バリデーションのテストを修正

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,19 +3,31 @@ RSpec.describe 'タスクモデル機能', type: :model do
   describe 'バリデーションのテスト' do
     context 'タスクのタイトルが空の場合' do
       it 'バリデーションにひっかる' do
-        task = Task.new(title: '', content: '失敗テスト')
+        task = Task.new(title: '', content: '失敗テスト', end_date: '2023-06-30', status: '失敗テスト')
         expect(task).not_to be_valid
       end
     end
-    context 'タスクの詳細が空の場合' do
+    context 'タスクの内容が空の場合' do
       it 'バリデーションにひっかかる' do
-        task = Task.new(title: '失敗テスト', content: '')
+        task = Task.new(title: '失敗テスト', content: '', end_date: '2023-06-30', status: '失敗テスト')
         expect(task).not_to be_valid
       end
     end
-    context 'タスクのタイトルと詳細に内容が記載されている場合' do
+    context 'タスクの終了期限が空の場合' do
+      it 'バリデーションにひっかかる' do
+        task = Task.new(title: '失敗テスト', content: '失敗テスト', end_date: '', status: '失敗テスト')
+        expect(task).not_to be_valid
+      end
+    end
+    context 'タスクのステータスが空の場合' do
+      it 'バリデーションにひっかかる' do
+        task = Task.new(title: '失敗テスト', content: '失敗テスト', end_date: '2023-06-30', status: '')
+        expect(task).not_to be_valid
+      end
+    end
+    context 'タスクのタイトルと詳細と終了期限とステータスに内容が記載されている場合' do
       it 'バリデーションが通る' do
-        task = Task.new(title: '成功テスト', content: '成功テスト')
+        task = Task.new(title: '成功テスト', content: '成功テスト', end_date: '2023-06-30', status: '成功テスト')
         expect(task).to be_valid
       end
     end
