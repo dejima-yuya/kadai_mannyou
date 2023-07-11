673ff6d4aa0d4e63d30c38d853830fd10d248500
dejima-yuya <jimajapan33@yahoo.co.jp>
2023-07-11 13:48:47 +0900

    アカウントを作成したとき、同時にログインもさせる

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
+      flash[:success] = 'アカウントを作成しました'
       redirect_to user_path(@user.id)
     else
       render :new
