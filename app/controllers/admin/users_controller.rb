983500c79ac6047f566df076485e7a075c81fc73
dejima-yuya <jimajapan33@yahoo.co.jp>
2023-07-12 14:36:53 +0900

    ユーザ作成機能を実装

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,27 @@
 class Admin::UsersController < ApplicationController
+  skip_before_action :login_required, only: [:new, :create]
+
   def index
     @users = User.select(:id, :name, :email)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'ユーザーを作成しました'
+      render :new
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email, :password, :password_confirmation)
   end
 end
