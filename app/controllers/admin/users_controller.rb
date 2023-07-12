0d6e2177e6a408e601f221f5461f013bc939be27
dejima-yuya <jimajapan33@yahoo.co.jp>
2023-07-12 18:52:38 +0900

    管理者がユーザの編集をできるようにする

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -19,6 +19,19 @@ class Admin::UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to edit_admin_user_path, notice: "ユーザーを編集しました"
+    else
+      render :edit
+    end
+  end
+
   private
 
   def user_params
