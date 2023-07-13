2a7a1676bf93d00fd38439cc8f42fad8c1b22d45
dejima-yuya <jimajapan33@yahoo.co.jp>
2023-07-13 17:21:11 +0900

    セッション機能のテストを実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,17 @@
 FactoryBot.define do
-  factory :user do
-    name { "一般ユーザ" }
-    email { "test@gmail.com" }
-    password { "testtest" }
+  factory :user1, class: User do
+    id { 1 }
+    name { '一般ユーザ' }
+    email { 'test@gmail.com' }
+    password { 'testtest' }
     admin { false }
+  end
+
+  factory :user2, class: User do
+    id { 2 }
+    name { "管理ユーザ" }
+    email { "admin@gmail.com" }
+    password { "testtest" }
+    admin { true }
   end
 end
--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
-RSpec.describe 'ユーザー登録機能', type: :system do
-  describe 'ユーザー作成機能' do
+RSpec.describe 'ユーザー管理機能', type: :system do
+  describe 'ユーザー登録機能' do
     context 'ユーザーを新規作成した場合' do
       it '作成したユーザーが表示される' do
         visit new_user_path
@@ -15,9 +15,46 @@ RSpec.describe 'ユーザー登録機能', type: :system do
     end
     context 'ユーザがログインせずタスク一覧画面に飛ぼうとした場合' do
       it 'ログイン画面に遷移する' do
-        user = FactoryBot.build(:user)
         visit tasks_path
         expect(page).to have_content 'ログイン'
+        expect(current_path).to eq new_session_path
+      end
+    end
+  end
+  describe 'セッション機能' do
+    before do
+      FactoryBot.create(:user1)
+      FactoryBot.create(:user2)
+    end
+    context 'ログインした場合' do
+      it 'ユーザーの詳細画面が表示される' do
+        visit new_session_path
+        fill_in 'session[email]', with: 'test@gmail.com'
+        fill_in 'session[password]', with: 'testtest'
+        click_on 'ログイン'
+        expect(page).to have_content '一般ユーザ'
+        expect(page).to have_content 'test@gmail.com'
+        expect(current_path).to eq user_path(1)
+      end
+    end
+    context '一般ユーザが他人の詳細画面に飛ぶと' do
+      it 'タスク一覧画面に遷移する' do
+        visit new_session_path
+        fill_in 'session[email]', with: 'test@gmail.com'
+        fill_in 'session[password]', with: 'testtest'
+        click_on 'ログイン'
+        visit user_path(2)
+        expect(current_path).to eq tasks_path
+      end
+    end
+    context 'ログアウトした場合' do
+      it 'ログイン画面が表示される' do
+        visit new_session_path
+        fill_in 'session[email]', with: 'test@gmail.com'
+        fill_in 'session[password]', with: 'testtest'
+        click_on 'ログイン'
+        click_on 'Logout'
+        expect(page).to have_content 'ログアウトしました'
         expect(current_path).to eq new_session_path
       end
     end
