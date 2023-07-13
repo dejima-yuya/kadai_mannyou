e5f042ad0df3c4e7f0f989589b343849ca3dce6f
dejima-yuya <jimajapan33@yahoo.co.jp>
2023-07-13 21:45:25 +0900

    タスクとユーザに アソシエーションが実装されたので、タスク管理機能のテストを修正

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,9 +1,33 @@
 FactoryBot.define do
-  factory :task do
-    title { 'テスト_タイトル' }
-    content { 'テスト_内容' }
+  factory :task1, class: Task do
+    title { 'テスト_タイトル1' }
+    content { 'テスト_内容1' }
     end_date { '2023-06-30' }
     status { '未着手' }
+    priority { '低' }
+  end
+
+  factory :task2, class: Task do
+    title { 'テスト_タイトル2' }
+    content { 'テスト_内容2' }
+    end_date { '2023-06-29' }
+    status { '着手中' }
+    priority { '中' }
+  end
+
+  factory :task3, class: Task do
+    title { 'テスト_タイトル3' }
+    content { 'テスト_内容3' }
+    end_date { '2023-06-28' }
+    status { '完了' }
     priority { '高' }
+  end
+
+  factory :user, class: User do
+    id { 1 }
+    name { 'user' }
+    email { 'test@gmail.com' }
+    password { 'testtest' }
+    admin { false }
   end
 end
